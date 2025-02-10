f581934b28af36b1e0e794cd657583505c0b7a00
fthuggy <179480003+fthuggy@users.noreply.github.com>
2025-02-10 22:30:40 +0100

    new style changes and idea of website

---
+++ portfolio/script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const introElements = document.querySelectorAll('.hide');
+introElements.forEach((el) => observer.observe(el));
+
